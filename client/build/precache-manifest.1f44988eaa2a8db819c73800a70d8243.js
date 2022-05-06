15c5888b2853787cb095f0ac00e133e8d84aabca
Victor Lindbloom <bkinrewin@gmail.com>
2022-05-05 21:00:37 -0400

    sever localhost run

---
+++ client/build/precache-manifest.1f44988eaa2a8db819c73800a70d8243.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2acb2213fc3d43624c3091545b5ce7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1efe74ba32ac3031852",
+    "url": "/static/css/main.e213c893.chunk.css"
+  },
+  {
+    "revision": "8816d3b662f423458fd0",
+    "url": "/static/js/2.deb5c141.chunk.js"
+  },
+  {
+    "revision": "cf1756f17a9fe3b6b59b94401b81c70b",
+    "url": "/static/js/2.deb5c141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1efe74ba32ac3031852",
+    "url": "/static/js/main.67e4c77a.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
